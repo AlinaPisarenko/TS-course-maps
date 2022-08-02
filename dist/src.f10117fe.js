93e494c6aa76cfe2953e3589f9ee1a445fce0c42
Alina <pisarenko.alina@gmail.com>
2022-08-02 18:04:09 -0400

    initialized Company Class

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -22773,6 +22773,32 @@ function () {
 }();
 
 exports.User = User;
+},{"@faker-js/faker":"node_modules/@faker-js/faker/dist/esm/index.mjs"}],"src/Company.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Company = void 0;
+
+var faker_1 = require("@faker-js/faker");
+
+var Company =
+/** @class */
+function () {
+  function Company() {
+    this.companyName = faker_1.faker.company.companyName();
+    this.catchPhrase = faker_1.faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker_1.faker.address.latitude()),
+      lng: parseFloat(faker_1.faker.address.longitude())
+    };
+  }
+
+  return Company;
+}();
+
+exports.Company = Company;
 },{"@faker-js/faker":"node_modules/@faker-js/faker/dist/esm/index.mjs"}],"src/index.ts":[function(require,module,exports) {
 "use strict";
 
@@ -22782,9 +22808,13 @@ Object.defineProperty(exports, "__esModule", {
 
 var User_1 = require("./User");
 
+var Company_1 = require("./Company");
+
 var user = new User_1.User();
 console.log(user);
-},{"./User":"src/User.ts"}],"../../../../.nvm/versions/node/v16.14.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var company = new Company_1.Company();
+console.log(company);
+},{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../../.nvm/versions/node/v16.14.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
